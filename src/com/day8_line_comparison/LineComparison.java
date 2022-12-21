9f8463e74d339c8b283367b243de2091377688f2
Sourav2707 <smartsourav33@gmail.com>
2022-12-21 19:45:28 +0530

    UC2 checking whether the two lines are equal

--- src/com/day8_line_comparison/LineComparison.java
+++ src/com/day8_line_comparison/LineComparison.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to line comparison program");
-        double lengthOfline;
+        double lengthOfline1, lengthOfline2;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the line point x1: ");
-        int x1 = sc.nextInt();
-        System.out.println("Enter the line point x2: ");
-        int x2 = sc.nextInt();
-        System.out.println("Enter the line point y1: ");
-        int y1 = sc.nextInt();
-        System.out.println("Enter the line point y2: ");
-        int y2 = sc.nextInt();
-        lengthOfline=(Math.sqrt( Math.pow((x2-x1),2) + Math.pow((y2-y1),2) ));
-        System.out.println("Length of line = "+lengthOfline);
+        System.out.println("Welcome to Line Comparison Computation");
+        System.out.println(" enter values for x1,y1 and x2,y2 of line 1");
+        lengthOfline1=getLength(sc.nextInt(), sc.nextInt(), sc.nextInt(),sc.nextInt());
+        System.out.println(" enter values for x1,y1 and x2,y2 of line 2");
+        lengthOfline2=getLength(sc.nextInt(), sc.nextInt(), sc.nextInt(),sc.nextInt());
+        System.out.println("Length of line1 = "+lengthOfline1);
+        System.out.println("Length of line2 = "+lengthOfline2);
+        if (lengthOfline1 == lengthOfline2) {
+            System.out.println("Lines are equal");
+        }
+        else
+            System.out.println("Lines are not equal");
+    }
+    static double getLength(double x1, double y1, double x2, double y2){
+        return ( Math.sqrt( Math.pow((x2-x1),2) + Math.pow((y2-y1),2) ) );
     }
 }
